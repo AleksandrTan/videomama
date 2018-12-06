b1025263c36fac759851fbddf42d666f7617d4f5
AleksandrTan <rumych@list.ru>
2018-12-06 17:59:19 +0200

    Messages storage

--- videomama/videomum/static/js/clientserver/socketserver.js
+++ videomama/videomum/static/js/clientserver/socketserver.js
@@ -37,7 +37,7 @@ sockConnect.onmessage = function(event) {
     }
     //Get users online every 10 seconds
     else if (answer.status == 6){
-        parseOnlineTimer(answer.online);
+        parseOnlineTimer(answer.online, answer.allcontacts);
     }
 };
 
@@ -74,7 +74,8 @@ function parseOnline(usersOnline = {}, allContacts = {}) {
         dataconnect.usersContacts = allContacts;
         parentList.empty();
         for(let key in allContacts){
-            parentList.append('<p id="'+key+'"><button type="button" class="btn btn-xs btn-info" data-id-omline="'+key+'">'+allContacts[key]+'</button></p>');
+            parentList.append('<p id="'+key+'"><button type="button" class="btn btn-xs btn-danger" ' +
+                              'data-id-omline="'+key+'">'+allContacts[key]+'</button></p>');
         }
     }
     else {
@@ -87,60 +88,66 @@ function parseOnline(usersOnline = {}, allContacts = {}) {
     if (!$.isEmptyObject(usersOnline)){
         dataconnect.usersOnline = usersOnline;
         for(let key in usersOnline){
-            if($("#"+key+"")){
-                $("#"+key+"").children('button').removeClass('btn-info').addClass('btn-success');
-            }
-            else {
-                $("#" + key + "").children('button').removeClass('btn-success').addClass('btn-info');
+            if($("#" + key + "")){
+                $('#'+key+'').find("button").removeClass('btn-danger').addClass('btn-success');
             }
         }
     }
 }
 
-//Check users online every 5 seconds
+//Check users online every 10 seconds
 setInterval(function () {
     sockConnect.send(prepareData(6, dataconnect.userId, 0, 'Get users online'));
 }, 10000);
 
 //Parse list users online every 10 seconds
-function parseOnlineTimer(usersData = {}) {
-    let parentList = $('#onlineList');
-    if ($.isEmptyObject(dataconnect.usersOnline)){
-        dataconnect.usersOnline = usersData;
-        parentList.empty();
-        for(let key in usersData){
-            parentList.append('<p id="'+key+'"><button type="button" class="btn btn-xs btn-success" data-id-omline="'+key+'">'+usersData[key]+'</button></p>');
-        }
+function parseOnlineTimer(usersOnline = {}, allContacts = {}) {
+    //if the user's contacts are not displayed yet
+    // (for example, the user had no contacts and he established a new contact)
+    if ($.isEmptyObject(dataconnect.usersContacts)){
+        parseOnline(usersOnline, allContacts);
         return;
     }
-    if (!$.isEmptyObject(usersData)){
-        //delete ofline users
-        for(let key in dataconnect.usersOnline){
-            if (key in usersData){
+    //check displayed contacts (someone added, someone removed)
+    if (!$.isEmptyObject(allContacts)){
+        //delete remove contacts
+        for(let key in dataconnect.usersContacts){
+            if(key in allContacts){
                 continue;
             }
             else {
-                delete dataconnect.usersOnline[key];
+                delete dataconnect.usersContacts[key];
                 $("#"+key+"").remove();
             }
         }
-        //add online users
-        for(let key in usersData){
-            if (key in dataconnect.usersOnline){
+        //add contacts
+        let parentList = $('#onlineList');
+        for (let key in allContacts){
+            if (key in dataconnect.usersContacts){
                 continue;
             }
             else {
-                dataconnect.usersOnline[key] = usersData[key];
-                parentList.append('<p id="'+key+'"><button type="button" class="btn btn-xs btn-success" data-id-omline="'+key+'">'+usersData[key]+'</button></p>');
+                dataconnect.usersContacts[key] = allContacts[key];
+                parentList.append('<p id="'+key+'"><button type="button" class="btn btn-xs btn-danger" ' +
+                                  'data-id-omline="'+key+'">'+allContacts[key]+'</button></p>');
             }
         }
-        console.log(dataconnect.usersOnline);
     }
-    else {
-        parentList.empty();
-        parentList.append('<p>There are no subscribers in the network</p>');
+    //check online status
+    dataconnect.usersOnline = usersOnline;
+    for(let key in allContacts){
+        if(key in usersOnline){
+            $('#'+key+'').find("button").removeClass('btn-danger').addClass('btn-success');;
+        }
+        else {
+            $('#'+key+'').find("button").removeClass('btn-success').addClass('btn-danger');
+        }
     }
+    console.log(dataconnect.usersOnline);
 }
+
+
+
 
 //Close connect with button
 $('#closeButton').on('click', function () {
